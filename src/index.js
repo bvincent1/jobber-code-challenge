a63e989f0da6026e3516250529026441be2a83cc
Ben Vincent <benjc.vincent@gmail.com>
2021-02-04 12:33:55 -0700

    Implement simple chained calculator

--- src/index.js
+++ src/index.js
@@ -1,3 +1,79 @@
+class NumbersText {
+  constructor() {
+    this.value = null;
+  }
+
+  addNumberToValue(newNumber) {
+    if (!this.value) {
+      this.value = newNumber;
+      return this;
+    } else {
+      return eval(`${this.value}${newNumber}`);
+    }
+  }
+
+  addOperatorToValue(newOperator) {
+    this.value = `${this.value}${newOperator}`;
+    return this;
+  }
+
+  get one() {
+    return this.addNumberToValue(1);
+  }
+
+  get two() {
+    return this.addNumberToValue(2);
+  }
+
+  get three() {
+    return this.addNumberToValue(3);
+  }
+
+  get four() {
+    return this.addNumberToValue(4);
+  }
+
+  get five() {
+    return this.addNumberToValue(5);
+  }
+
+  get six() {
+    return this.addNumberToValue(6);
+  }
+
+  get seven() {
+    return this.addNumberToValue(7);
+  }
+
+  get eight() {
+    return this.addNumberToValue(8);
+  }
+
+  get nine() {
+    return this.addNumberToValue(9);
+  }
+
+  get plus() {
+    return this.addOperatorToValue("+");
+  }
+
+  get minus() {
+    return this.addOperatorToValue("-");
+  }
+
+  get multiply() {
+    return this.addOperatorToValue("*");
+  }
+
+  get divide_by() {
+    return this.addOperatorToValue("/");
+  }
+}
+
 const Calc = {
-    new: new Number()
+  get new() {
+    return new NumbersText();
+  },
 };
+
+export default Calc;
--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,11 @@ import Calc from "./index";
 
 describe("Cacl", () => {
   it("should return 1", () => {
-    expect(Calc.new.one.plus.one).toBe(1);
+    expect(Calc.new.one).toEqual({ value: 1 });
+  });
+
+  it("should return 2", () => {
+    expect(Calc.new.one.plus.one).toBe(2);
   });
 
   it("should return 18", () => {
@@ -10,14 +14,14 @@ describe("Cacl", () => {
   });
 
   it("should return -5", () => {
-    expect(Calc.new.one.minus.six).toBe(6);
+    expect(Calc.new.one.minus.six).toBe(-5);
   });
 
   it("should return -0.33333", () => {
-    expect(Calc.new.one.divided_by.three).toBe(1/3);
+    expect(Calc.new.one.divide_by.three).toBe(1 / 3);
   });
 
-  it("should return Syntax Error", () => {
-    expect(Calc.new.one.divided_by).toEqual(Calc.SyntaxError)
+  it("should return current value", () => {
+    expect(Calc.new.one.divide_by).toEqual({ value: "1/" });
   });
 });
